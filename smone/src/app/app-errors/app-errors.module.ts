be3982f6cbeb65340f399e0f54d385d179101667
unknown <mukesh214@hotmail.com>
2023-08-17 11:14:13 +0530

    login forms

---
+++ smone/src/app/app-errors/app-errors.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GlobalErrorComponent } from './global-error/global-error.component';
+
+
+
+@NgModule({
+  declarations: [GlobalErrorComponent,],
+  imports: [
+    CommonModule
+  ],
+  exports:[GlobalErrorComponent,]
+})
+export class AppErrorsModule { }
